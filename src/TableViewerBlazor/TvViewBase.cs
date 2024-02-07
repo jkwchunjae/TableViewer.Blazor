1d614d4f0a6dd60f0dc6510d2a692db3dea53840
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-07 16:03:31 +0900

    TvViewBase

---
+++ src/TableViewerBlazor/TvViewBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TableViewerBlazor;
+
+public class TvViewBase : ComponentBase
+{
+    [Parameter] public TvOptions? Options { get; set; }
+    [Parameter] public int Depth { get; set; }
+}
