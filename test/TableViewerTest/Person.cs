4ea17184fc8a9434b05610e21728a5e74ff06314
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-07 16:06:44 +0900

    테스트 샘플

---
+++ test/TableViewerTest/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace TableViewerTest;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Dictionary<string, object> Attributes { get; set; }
+}
