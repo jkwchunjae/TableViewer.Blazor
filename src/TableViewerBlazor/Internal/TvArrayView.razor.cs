810e313af87773ca926f600e0fdcaf5ac99f3e48
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-08 06:37:39 +0900

    TvArrayView 초안

---
+++ src/TableViewerBlazor/Internal/TvArrayView.razor.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace TableViewerBlazor.Internal;
+
+public partial class TvArrayView : TvViewBase
+{
+    [Parameter] public IEnumerable Data { get; set; } = null!;
+
+    private IEnumerable<object?> all;
+    private IEnumerable<(int Index, object? Item)> DataArray;
+    int VisibleItems = 2;
+    bool Open = false;
+
+    bool HasMoreItems => DataArray.Count() < all.Count();
+
+    protected override void OnInitialized()
+    {
+        if (Options != null)
+        {
+            Open = Depth <= Options.OpenDepth;
+            if (VisibleItems < Options.ArrayVisibleDepth)
+            {
+                VisibleItems = Options.ArrayVisibleDepth;
+            }
+            all = Data
+                .Cast<object?>();
+            DataArray = all
+                .Select((item, index) => (index, item))
+                .Take(VisibleItems);
+        }
+    }
+
+    private void MoreItems()
+    {
+        VisibleItems *= 2;
+        DataArray = all
+            .Select((item, index) => (index, item))
+            .Take(VisibleItems);
+        StateHasChanged();
+    }
+
+    private void ToggleOpen()
+    {
+        Open = !Open;
+    }
+}
+
+// https://stackoverflow.com/questions/4015930/when-to-use-cast-and-oftype-in-linq
