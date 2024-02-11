ac8c68d785b20e8c43f17cd39ce54716208b3b13
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-11 15:03:14 +0900

    IsObjectArray

--- src/TableViewerBlazor/Internal/TvElementView.razor.cs
+++ src/TableViewerBlazor/Internal/TvElementView.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
 
 namespace TableViewerBlazor.Internal;
 
@@ -17,4 +19,34 @@ public partial class TvElementView : TvViewBase
         decimal => true,
         _ => false,
     };
+
+    private bool IsObjectArray
+    {
+        get {
+            // if (Data?.GetType()?.IsArray ?? false)
+            // {
+            //     return Data.GetType().GetElementType()?.IsClass ?? false;
+            // }
+            // if (Data?.GetType()?.GenericTypeArguments?.Length > 0)
+            // {
+            //     return Data!.GetType().GenericTypeArguments[0].IsClass;
+            // }
+            if (Data is IEnumerable arr)
+            {
+                var firstData = arr.Cast<object>().FirstOrDefault();
+                if (firstData != null)
+                {
+                    return firstData!.GetType().IsClass;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 }
