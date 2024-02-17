209e5da6ae967b1fd4ec76f7d027a60ab111822c
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-17 11:11:47 +0900

    test code

--- test/TableViewerTest/Person.cs
+++ test/TableViewerTest/Person.cs
@@ -4,7 +4,7 @@ public class Person
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public Dictionary<string, object> Attributes { get; set; }
+    public Dictionary<string, object>? Attributes { get; set; }
 }
 
-public record PersonRecord(int Id, string? Name, Dictionary<string, object> Attributes);
+public record PersonRecord(int Id, string? Name, Dictionary<string, object>? Attributes);
