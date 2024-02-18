d263bbef11d8f2877a77e3bb56d42f92babd8b30
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-19 00:25:22 +0900

    #1 action in array, object-array

--- src/TableViewerBlazor/Options/TvOptions.cs
+++ src/TableViewerBlazor/Options/TvOptions.cs
@@ -4,4 +4,26 @@ public class TvOptions
 {
     public int OpenDepth { get; set; } = 1;
     public int ArrayVisibleDepth { get; set; } = 1;
+    public IEnumerable<ITvAction>? Actions { get; set; }
+}
+
+public interface ITvAction
+{
+    public Func<object?, int, bool> Condition { get; }
+    public Func<object?, Task> Action { get; }
+    public string Label { get; }
+    public string LabelAfterClick { get; }
+    public string Class { get; }
+}
+
+public class TvAction<T> : ITvAction
+{
+    public Func<T , int, bool> Condition { get; set; } = (_, _) => true;
+    public Func<T, Task> Action { get; set; } = _ => Task.CompletedTask;
+    public string Label { get; set; } = "ACTION";
+    public string LabelAfterClick { get; set; } = "DONE";
+    public string Class { get; set; } = string.Empty;
+
+    Func<object?, int, bool> ITvAction.Condition => (o, i) => o is T t && Condition(t, i);
+    Func<object?, Task> ITvAction.Action => (o) => o is T t ? Action(t) : Task.CompletedTask;
 }
--- test/TableViewerTest/Person.cs
+++ test/TableViewerTest/Person.cs
@@ -5,6 +5,8 @@ public class Person
     public int Id { get; set; }
     public string? Name { get; set; }
     public Dictionary<string, object>? Attributes { get; set; }
+
+    public PersonRecord[] People { get; set; } = Array.Empty<PersonRecord>();
 }
 
 public record PersonRecord(int Id, string? Name, Dictionary<string, object>? Attributes);
