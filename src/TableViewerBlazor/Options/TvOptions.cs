7f986d9e3ae394af5ca512542456fbc3331ca24d
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-19 22:40:03 +0900

    TvAction 파일 분리

--- src/TableViewerBlazor/Options/TvOptions.cs
+++ src/TableViewerBlazor/Options/TvOptions.cs
@@ -4,26 +4,7 @@ public class TvOptions
 {
     public int OpenDepth { get; set; } = 1;
     public int ArrayVisibleDepth { get; set; } = 1;
+    public bool ReadProperty { get; set; } = true;
+    public bool ReadField { get; set; } = true;
     public IEnumerable<ITvAction>? Actions { get; set; }
-}
-
-public interface ITvAction
-{
-    public Func<object?, int, bool> Condition { get; }
-    public Func<object?, Task> Action { get; }
-    public string Label { get; }
-    public string LabelAfterClick { get; }
-    public string Class { get; }
-}
-
-public class TvAction<T> : ITvAction
-{
-    public Func<T , int, bool> Condition { get; set; } = (_, _) => true;
-    public Func<T, Task> Action { get; set; } = _ => Task.CompletedTask;
-    public string Label { get; set; } = "ACTION";
-    public string LabelAfterClick { get; set; } = "DONE";
-    public string Class { get; set; } = string.Empty;
-
-    Func<object?, int, bool> ITvAction.Condition => (o, i) => o is T t && Condition(t, i);
-    Func<object?, Task> ITvAction.Action => (o) => o is T t ? Action(t) : Task.CompletedTask;
 }
