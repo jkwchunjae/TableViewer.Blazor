e25e0921cfaf0167b1eb4aa41820d23a197ced21
Kyungwon Jeon <jkwchunjae@daum.net>
2024-02-20 13:17:52 +0900

    #4 Dictionary view

---
+++ src/TableViewerBlazor/Internal/TvDictionaryView.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections;
+using System.Reflection;
+
+namespace TableViewerBlazor.Internal;
+
+public partial class TvDictionaryView : TvViewBase
+{
+    [Parameter] public IDictionary Data { get; set; } = null!;
+
+    bool Open = false;
+
+    private IEnumerable<(object? Key, object? Value)> Items = Enumerable.Empty<(object?, object?)>();
+
+    protected override void OnInitialized()
+    {
+        if (Options != null)
+        {
+            Open = Depth <= Options.OpenDepth;
+        }
+        if (Data != null)
+        {
+            Items = Convert(Data);
+        }
+    }
+
+    private IEnumerable<(object? Key, object? Value)> Convert(IDictionary data)
+    {
+        foreach (var key in data.Keys)
+        {
+            yield return (key, data[key]);
+        }
+    }
+
+    private void ToggleOpen()
+    {
+        Open = !Open;
+    }
+}
