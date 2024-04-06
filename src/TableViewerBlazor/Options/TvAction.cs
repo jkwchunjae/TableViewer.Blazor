f420842fe90ba8529416f34544f8cbd9cd90d51e
Kyungwon Jeon <jkwchunjae@daum.net>
2024-04-06 23:34:11 +0900

    MudButton.ActionButton

--- src/TableViewerBlazor/Options/TvAction.cs
+++ src/TableViewerBlazor/Options/TvAction.cs
@@ -1,3 +1,5 @@
+using MudBlazor;
+
 namespace TableViewerBlazor.Options;
 
 public interface ITvAction
@@ -6,7 +8,7 @@ public interface ITvAction
     public Func<object?, Task> Action { get; }
     public string Label { get; }
     public string LabelAfterClick { get; }
-    public string Class { get; }
+    public TvButtonStyle Style { get; }
 }
 
 public class TvAction<T> : ITvAction
@@ -15,8 +17,20 @@ public class TvAction<T> : ITvAction
     public Func<T, Task> Action { get; set; } = _ => Task.CompletedTask;
     public string Label { get; set; } = "ACTION";
     public string LabelAfterClick { get; set; } = "DONE";
-    public string Class { get; set; } = string.Empty;
+    public TvButtonStyle Style { get; set; } = new();
 
     Func<object?, int, bool> ITvAction.Condition => (o, i) => o is T t && Condition(t, i);
     Func<object?, Task> ITvAction.Action => (o) => o is T t ? Action(t) : Task.CompletedTask;
+}
+
+public class TvButtonStyle
+{
+    public Variant Variant { get; set; } = Variant.Outlined;
+    public Size Size { get; set; } = Size.Small;
+    public Color Color { get; set; } = Color.Default;
+    public string Style { get; set; } = "text-transform: none;";
+    public string? StartIcon { get; set; } = null;
+    public string? EndIcon { get; set; } = null;
+    public Size IconSize { get; set; } = Size.Small;
+    public Color IconColor { get; set; } = Color.Default;
 }
--- src/TableViewerBlazor/Options/TvOptions.cs
+++ src/TableViewerBlazor/Options/TvOptions.cs
@@ -15,7 +15,7 @@ public class TvOptions
 public class TvStyleOption
 {
     public bool Dense { get; set; } = true;
-    public bool Hover { get; set; } = true;
+    public bool Hover { get; set; } = false;
     public bool Striped { get; set; } = false;
     public bool Bordered { get; set; } = true;
 }
