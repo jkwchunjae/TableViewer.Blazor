7fcee3dab76f55a94c1a9a60936d149543c1f6b5
Kyungwon Jeon <jkwchunjae@daum.net>
2024-04-09 11:18:12 +0900

    DoubleClickButton

--- src/TableViewerBlazor/Options/TvDoubleClickAction.cs
+++ src/TableViewerBlazor/Options/TvDoubleClickAction.cs
@@ -1,13 +1,22 @@
+using MudBlazor;
+
 namespace TableViewerBlazor.Options;
 
 public interface ITvDoubleClickAction : ITvAction
 {
-    public TvButtonStyle InitStyle { get; }
+    public TvSecondButtonStyle SecondStyle { get; }
     public TimeSpan ResetDelay { get; }
 }
 public class TvDoubleClickAction<T> : TvAction<T>, ITvDoubleClickAction
 {
-    public TvButtonStyle InitStyle { get; set; } = new();
+    public TvSecondButtonStyle SecondStyle { get; set; } = new();
 
     public TimeSpan ResetDelay { get; set; } = TimeSpan.FromSeconds(2);
+}
+
+public class TvSecondButtonStyle
+{
+    public Variant Variant { get; set; } = Variant.Outlined;
+    public Color Color { get; set; } = Color.Default;
+    public Color IconColor { get; set; } = Color.Default;
 }
