42d095255fac6dbf9221b22b46e96751bfb83c0f
Kyungwon Jeon <jkwchunjae@daum.net>
2024-04-15 13:29:40 +0900

    editor option

--- src/TableViewerBlazor/Internal/TvComponent/TvMonacoEditorViewer.razor.cs
+++ src/TableViewerBlazor/Internal/TvComponent/TvMonacoEditorViewer.razor.cs
@@ -1,17 +1,34 @@
 using System.Text.Json;
 using BlazorMonaco.Editor;
+using Microsoft.JSInterop;
 
 namespace TableViewerBlazor.Internal.TvComponent;
 
 public partial class TvMonacoEditorViewer : TvViewBase
 {
+    [Inject] public IJSRuntime Js { get; set; } = null!;
     [Parameter] public object? Data { get; set; }
+    [Parameter] public ITvEditorOption EditorOption { get; set; } = null!;
+
     static Random random = new Random((int)DateTime.Now.Ticks);
     string EditorId = $"tv-monaco-{random.Next(11111, 99999)}";
     StandaloneCodeEditor Editor { get; set; } = null!;
 
+    int height = 1;
+    int width = 1;
+
     protected override Task OnInitializedAsync()
     {
+        InvokeAsync(async () =>
+        {
+            await Task.Delay(10);
+            await Editor.Layout(new Dimension
+            {
+                Height = height * 20,
+                Width = width * 10,
+            });
+            StateHasChanged();
+        });
         return base.OnInitializedAsync();
     }
 
@@ -21,16 +38,18 @@ public partial class TvMonacoEditorViewer : TvViewBase
         {
             WriteIndented = true,
         });
-        var serializer = Options.Editor.Serializer ?? func;
+        var serializer = EditorOption.Serializer ?? func;
         var value = serializer(Data!);
         var lines = value.Split('\n');
         var lineCount = lines.Count();
         var maxWidth = lines.Max(x => x.Length);
+        height = lineCount;
+        width = maxWidth;
 
         return new StandaloneEditorConstructionOptions
         {
             AutomaticLayout = true,
-            Language = Options.Editor.Language,
+            Language = EditorOption.Language,
             Value = value,
             ReadOnly = true,
             LineNumbers = RenderLineNumbersType.Off.ToString(),
--- src/TableViewerBlazor/Options/TvEditorOption.cs
+++ src/TableViewerBlazor/Options/TvEditorOption.cs
@@ -1,7 +1,23 @@
 namespace TableViewerBlazor.Options;
 
-public class TvEditorOption
+public interface ITvEditorOption
 {
+    delegate bool EditorOptionCondition(object? data, int depth, string path);
+    string Language { get; }
+    Func<object, string>? Serializer { get; }
+    EditorOptionCondition? Condition { get; }
+}
+
+public class TvEditorOption<T> : ITvEditorOption
+{
+    public delegate bool EditorOptionConditionT(T? data, int depth, string path);
     public string Language { get; set; } = "json";
-    public Func<object, string>? Serializer { get; set; }
+    public Func<T, string>? Serializer { get; set; }
+    public Func<T?, int, string, bool>? Condition { get; set; }
+
+    ITvEditorOption.EditorOptionCondition? ITvEditorOption.Condition =>
+        (data, depth, path) => data is T t && Condition != null ? Condition(t, depth, path) : false;
+
+    Func<object, string>? ITvEditorOption.Serializer =>
+        (data) => data is T t && Serializer != null ? Serializer.Invoke(t) : string.Empty;
 }
