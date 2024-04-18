f9f78905227cc76e93556c5713cd737d26e4a7f9
Kyungwon Jeon <jkwchunjae@daum.net>
2024-04-18 09:29:43 +0900

    ITvOpenDepthOption

--- src/TableViewerBlazor/Internal/TvComponent/TvViewBase.cs
+++ src/TableViewerBlazor/Internal/TvComponent/TvViewBase.cs
@@ -7,4 +7,5 @@ public class TvViewBase : ComponentBase
 {
     [Parameter] public TvOptions Options { get; set; } = new TvOptions();
     [Parameter] public int Depth { get; set; }
+    [Parameter] public int OpenDepth { get; set; }
 }
