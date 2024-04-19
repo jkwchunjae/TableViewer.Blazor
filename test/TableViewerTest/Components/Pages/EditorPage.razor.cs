2bfbeada63c4c1787109dfd14c066091081705ef
Kyungwon Jeon <jkwchunjae@daum.net>
2024-04-20 01:37:57 +0900

    sample

---
+++ test/TableViewerTest/Components/Pages/EditorPage.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace TableViewerTest.Components.Pages;
+
+public class EditData
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
+
+public partial class EditorPage : ComponentBase
+{
+    [Inject] private IJSRuntime Js { get; set; } = default!;
+
+    EditData data = new EditData
+    {
+        Name = "John",
+        Age = 30,
+    };
+    private async Task Changed(EditData data)
+    {
+        await Js.InvokeVoidAsync("console.log", data);
+    }
+}
