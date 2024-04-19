38436b55a5731870e304bc54f8004294f2636b94
Kyungwon Jeon <jkwchunjae@daum.net>
2024-04-20 01:04:16 +0900

    Init TableEditor component

---
+++ src/TableViewerBlazor/TableEditor.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace TableViewerBlazor;
+
+public partial class TableEditor<T> : ComponentBase
+{
+    [Parameter] public T Data { get; set; } = default!;
+    [Parameter] public EventCallback<T> DataChanged { get; set; }
+
+    private async Task TestClick()
+    {
+        await DataChanged.InvokeAsync(Data);
+    }
+}
