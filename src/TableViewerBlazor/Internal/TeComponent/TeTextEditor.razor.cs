bac9f72aeb74f4a5011f681f54fa1833645c5223
Kyungwon Jeon <jkwchunjae@daum.net>
2024-04-21 23:25:17 +0900

    TeTextEditor

---
+++ src/TableViewerBlazor/Internal/TeComponent/TeTextEditor.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace TableViewerBlazor.Internal.TeComponent;
+
+public partial class TeTextEditor : TeEditorBase
+{
+    private async Task OnValueChanged(object? value)
+    {
+        Data = value;
+        await DataChanged.InvokeAsync(value);
+    }
+}
