cccb5c482036c2bdfc841709a9259dba0bdb0186
dean.kim <dean.kim@pubg.com>
2024-07-04 10:50:04 +0900

    Revert "Move dateTime convert logic to DateTimeService"
    This reverts commit 33ac01b7d4657eadc922a624e3e186f061b1bca2.

--- src/TableViewerBlazor/Internal/TvComponent/TvDateTimeView.razor.cs
+++ src/TableViewerBlazor/Internal/TvComponent/TvDateTimeView.razor.cs
@@ -12,9 +12,40 @@ public partial class TvDateTimeView : TvViewBase
 
     protected override void OnParametersSet()
     {
-        if (Options?.DateTime != null)
+        var targetDateTime = Data.ToUniversalTime();
+        var options = DateTimeService.Options;
+        // NOTE: 타이밍 이슈로 항상 options == null인 상황
+        if (options != null)
         {
-            dateTime = DateTimeService.ConvertDateTimeFormat(Data, Options.DateTime);
+            targetDateTime = targetDateTime.AddHours(-(options.Offset / 60));
+        }
+        dateTime = ConvertDateTime(targetDateTime);
+    }
+
+    private string ConvertDateTime(DateTime targetDateTime)
+    {
+        var dateTimeOption = Options?.DateTime;
+        if (dateTimeOption != null)
+        {
+            if (dateTimeOption.RelativeTime)
+            {
+                if (targetDateTime > DateTime.Now)
+                {
+                    return targetDateTime.ToNow();
+                }
+                else
+                {
+                    return targetDateTime.FromNow();
+                }
+            }
+            else
+            {
+                return targetDateTime.ToString(dateTimeOption.Format);
+            }
+        }
+        else
+        {
+            return "";
         }
     }
 }
--- src/TableViewerBlazor/Service/DateTimeService.cs
+++ src/TableViewerBlazor/Service/DateTimeService.cs
@@ -1,37 +1,8 @@
-﻿using moment.net;
+﻿using TableViewerBlazor.Options;
 
 namespace TableViewerBlazor.Service;
 
 internal class DateTimeService
 {
-    public TvDateTimeOption? GlobalOption { get; set; }
-
-    public static string ConvertDateTimeFormat(DateTime dateTime, TvDateTimeOption option)
-    {
-        dateTime = dateTime.ToLocalTime();
-
-        if (option.RelativeTime)
-        {
-            return ConvertToRelativeTime(dateTime);
-        }
-        else
-        {
-            return dateTime.ToString(option.Format);
-        }
-    }
-
-
-    private static string ConvertToRelativeTime(DateTime dateTime)
-    {
-        if (dateTime > DateTime.Now)
-        {
-            return dateTime.ToNow();
-        }
-        else
-        {
-            return dateTime.FromNow();
-        }
-    }
-
-    
+    public TvDateTimeOption? Options { get; set; }
 }
