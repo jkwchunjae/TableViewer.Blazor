e6f0871bd780101a1c1487abd24b3c1f059c98df
Kyungwon Jeon <jkwchunjae@daum.net>
2024-07-27 23:42:49 +0900

    remove link from action

--- src/TableViewerBlazor/Options/TvAction.cs
+++ src/TableViewerBlazor/Options/TvAction.cs
@@ -7,7 +7,6 @@ public interface ITvAction
     public string Label { get; }
     public string LabelAfterClick { get; }
     public TvButtonStyle Style { get; }
-    public ILink? Link { get; }
 }
 
 public class TvAction<T> : ITvAction
@@ -17,7 +16,6 @@ public class TvAction<T> : ITvAction
     public string Label { get; set; } = "ACTION";
     public string LabelAfterClick { get; set; } = "DONE";
     public TvButtonStyle Style { get; set; } = new();
-    public ILink? Link { get; set; }
 
     Func<object?, int, bool> ITvAction.Condition => (o, i) => o is T t && Condition(t, i);
     Func<object?, Task> ITvAction.Action => (o) => o is T t ? Action(t) : Task.CompletedTask;
@@ -35,25 +33,4 @@ public class TvButtonStyle
     public Color IconColor { get; set; } = Color.Default;
     public bool Dense { get; set; } = true;
     public bool SuperDense { get; set; } = false;
-}
-
-public interface ILink
-{
-    string Target { get; }
-    public Func<object?, string> Href { get; }
-}
-
-public class Link<T> : ILink
-{
-    public string Target { get; set; } = "_self";
-    public required Func<T, string> Href { get; set; }
-    Func<object?, string> ILink.Href => o => o is T t ? Href(t) : string.Empty;
-}
-
-public class Link : ILink
-{
-    public string Target { get; set; } = "_self";
-    public required string Href { get; set; }
-
-    Func<object?, string> ILink.Href => _ => Href;
 }
