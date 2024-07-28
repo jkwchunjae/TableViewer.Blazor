a8c8c835d9e955ee05374d23c016f1d57ebabebd
Kyungwon Jeon <jkwchunjae@daum.net>
2024-07-28 23:26:13 +0900

    Style.Hover

--- src/TableViewerBlazor/Options/TeStyleOption.cs
+++ src/TableViewerBlazor/Options/TeStyleOption.cs
@@ -4,6 +4,7 @@ public class TeStyleOption
 {
     public bool Dense { get; set; } = true;
     public bool SuperDense { get; set; } = true;
+    public bool Hover { get; set; } = false;
     public bool Striped { get; set; } = false;
     public bool Bordered { get; set; } = true;
     public List<string> RootClass { get; set; } = [];
