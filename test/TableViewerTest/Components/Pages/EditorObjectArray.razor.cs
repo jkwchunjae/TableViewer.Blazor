55644fc6d213911e94cb5ea82a2bfc460c1d3789
Kyungwon Jeon <jkwchunjae@daum.net>
2024-07-28 15:42:56 +0900

    test page

--- test/TableViewerTest/Components/Pages/EditorObjectArray.razor.cs
+++ test/TableViewerTest/Components/Pages/EditorObjectArray.razor.cs
@@ -1,8 +1,26 @@
 
 using Microsoft.AspNetCore.Components;
+using TableViewerBlazor.Options;
 
 namespace TableViewerTest.Components.Pages;
 
+class EditItem
+{
+    public PersonName? Name { get; set; }
+    public int Age { get; set; }
+    public DateTime Birth { get; set; }
+}
+
 public partial class EditorObjectArray : ComponentBase
 {
+    TeOptions? options = default;
+    List<EditItem> items = new List<EditItem>();
+    protected override Task OnInitializedAsync()
+    {
+        options = new TeOptions
+        {
+            
+        };
+        return base.OnInitializedAsync();
+    }
 }
