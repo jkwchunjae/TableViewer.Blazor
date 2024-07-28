0edcc9cd1f095f4a520c0f075d0dc177c55d1359
Kyungwon Jeon <jkwchunjae@daum.net>
2024-07-28 17:50:18 +0900

    minor

--- src/TableViewerBlazor/Internal/TeComponent/TeSelectBox.razor.cs
+++ src/TableViewerBlazor/Internal/TeComponent/TeSelectBox.razor.cs
@@ -4,7 +4,6 @@ namespace TableViewerBlazor.Internal.TeComponent;
 
 public partial class TeSelectBox : TeEditorBase
 {
-    [Inject] IJSRuntime Js { get; set; } = default!;
     [Parameter] public ITeSelectBoxOption SelectBoxOption { get; set; } = default!;
 
     private async Task OnValueChanged(object? value)
