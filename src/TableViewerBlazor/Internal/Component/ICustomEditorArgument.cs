1c5ba4eefd20e3b5b57314fe2f1f51a780e501d6
Kyungwon Jeon <jkwchunjae@daum.net>
2024-07-30 13:58:36 +0900

    Create ICustomEditorArgument.cs

---
+++ src/TableViewerBlazor/Internal/Component/ICustomEditorArgument.cs
@@ -0,0 +1,17 @@
+﻿namespace TableViewerBlazor.Internal.Component;
+
+public interface ICustomEditorArgument
+{
+    event EventHandler<object> ParentChanged;
+    object? Parent { get; }
+    object? Value { get; }
+    Func<object?, Task> DataChanged { get; }
+}
+
+public class CustomEditorArguemnt : ICustomEditorArgument
+{
+    public event EventHandler<object> ParentChanged;
+    public object? Parent { get; set; }
+    public object? Value { get; set; }
+    public Func<object?, Task> DataChanged { get; set; } = default!;
+}
