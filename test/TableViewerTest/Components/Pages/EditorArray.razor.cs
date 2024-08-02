9e773af2bb6caeb34e1b212bf060ebf2f74b5566
dean.kim <dean.kim@pubg.com>
2024-08-02 12:43:09 +0900

    bool  처리

--- test/TableViewerTest/Components/Pages/EditorArray.razor.cs
+++ test/TableViewerTest/Components/Pages/EditorArray.razor.cs
@@ -7,9 +7,9 @@ namespace TableViewerTest.Components.Pages;
 public partial class EditorArray : ComponentBase
 {
     [Inject] private IJSRuntime Js { get; set; } = default!;
-    //private readonly List<string> arrayData = ["1", "2", "배그", "bitter"];
+    private readonly List<string> arrayData = ["1", "2", "배그", "bitter"];
     //private readonly List<bool> arrayData = [false];
-    private readonly List<object> arrayData = ["1", 2, "배그", "bitter"];
+    //private readonly List<object> arrayData = ["1", 2, "배그", "bitter", false];
     //private readonly List<object> arrayData = ["1", 2, "배그", "bitter", false, new TeOptions()];
     private readonly TeOptions teOptions = new()
     {
@@ -22,6 +22,18 @@ public partial class EditorArray : ComponentBase
                 EnableAddItem = true,
             }
         ],
+        TextFieldOptions =
+        {
+            new TeTextFieldOption<object>
+            {
+                Condition = (obj, _, _) => obj is bool,
+                Converter = new TeTextFieldConverter<object>
+                {
+                    FromString = s => bool.TryParse(s, out var boolValue) ? boolValue : false,
+                    StringValue = p =>  p?.ToString() ?? string.Empty,
+                }
+            },
+        }
         //CheckBoxOptions =
         //[
         //    new TeCheckBoxOption
@@ -38,7 +50,7 @@ public partial class EditorArray : ComponentBase
         //]
     };
 
-    private async Task OnDataChanged(List<object> data)
+    private async Task OnDataChanged(List<string> data)
     {
         await Js.InvokeVoidAsync("console.log", data);
     }
