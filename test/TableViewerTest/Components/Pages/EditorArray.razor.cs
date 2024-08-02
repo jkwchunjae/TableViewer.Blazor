4af0f7c523feaf993b757cb97169fadabde88258
Kyungwon Jeon <jkwchunjae@daum.net>
2024-08-02 22:02:56 +0900

    test page

--- test/TableViewerTest/Components/Pages/EditorArray.razor.cs
+++ test/TableViewerTest/Components/Pages/EditorArray.razor.cs
@@ -9,30 +9,20 @@ public partial class EditorArray : ComponentBase
     [Inject] private IJSRuntime Js { get; set; } = default!;
     private readonly List<string> arrayData = ["1", "2", "배그", "bitter"];
     private List<int> intList = new() { 123, 4567, };
-    //private readonly List<bool> arrayData = [false];
+    private List<bool> boolList = new() { true, false, };
     //private readonly List<object> arrayData = ["1", 2, "배그", "bitter", false];
     //private readonly List<object> arrayData = ["1", 2, "배그", "bitter", false, new TeOptions()];
     private readonly TeOptions teOptions = new()
     {
         Title = "string list",
-        //CheckBoxOptions =
-        //[
-        //    new TeCheckBoxOption
-        //    {
-        //        Property = new TeCheckBoxProperty
-        //        {
-        //            Color = MudBlazor.Color.Primary,
-        //            //LabelOptions = new TeLabelOptions<>
-        //            //{
-        //            //    //SelectedLabel
-        //            //}
-        //        }
-        //    }
-        //]
     };
     private TeOptions intOptions = new()
     {
         Title = "int list",
+    };
+    private TeOptions boolOptions = new()
+    {
+        Title = "bool list",
     };
 
     private async Task OnDataChanged<T>(T data)
