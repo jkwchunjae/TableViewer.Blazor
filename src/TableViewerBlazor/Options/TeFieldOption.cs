975e141a05fa1cb5f27a920ddd2e260c871485eb
Kyungwon Jeon <jkwchunjae@daum.net>
2024-08-03 16:40:58 +0900

    ITeConverter

--- src/TableViewerBlazor/Options/TeFieldOption.cs
+++ src/TableViewerBlazor/Options/TeFieldOption.cs
@@ -10,7 +10,7 @@ public interface ITeFieldOption
 public interface ITeFieldOption<TValue> : ITeFieldOption
 {
     new Func<TValue?, int, string, bool>? Condition { get; }
-    new ITeConverter<TValue> Converter { get; }
+    new ITeConverter<TValue, object> Converter { get; }
 
     Func<object?, int, string, bool>? ITeFieldOption.Condition =>
         (value, index, path) =>
@@ -25,15 +25,35 @@ public interface ITeFieldOption<TValue> : ITeFieldOption
             }
         };
 }
+public interface ITeFieldOption<TValue, TField> : ITeFieldOption
+{
+    new ITeConverter<TValue, TField> Converter { get; }
+}
 
 public interface ITeConverter
 {
-    Func<object, object> ToField { get; }
-    Func<object, object> FromField { get; }
+    Func<object, object?> ToField { get; }
+    Func<object, object?> FromField { get; }
 }
 
-public interface ITeConverter<TValue> : ITeConverter
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="TValue">유저가 가지고 있는 데이터 타입</typeparam>
+/// <typeparam name="TField">TableEditor쪽 데이터 타입</typeparam>
+public interface ITeConverter<TValue, TField> : ITeConverter
 {
-    new Func<TValue, object> ToField { get; }
-    new Func<object, TValue> FromField { get; }
+    new Func<TValue, TField?> ToField { get; }
+    new Func<TField, TValue?> FromField { get; }
+}
+
+public class TeConverter<TValue, TField> : ITeConverter<TValue, TField>
+{
+    public required Func<TValue, TField?> ToField { get; set; }
+
+    public required Func<TField, TValue?> FromField { get; set; }
+
+    Func<object, object?> ITeConverter.ToField => userValue => userValue is TValue value ? ToField(value) : default;
+
+    Func<object, object?> ITeConverter.FromField => fieldValue => fieldValue is TField value ? FromField(value) : default;
 }
