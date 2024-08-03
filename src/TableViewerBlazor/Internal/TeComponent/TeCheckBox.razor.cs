a410794b4845f711de712f5209347a5db020910e
Kyungwon Jeon <jkwchunjae@daum.net>
2024-08-04 00:37:24 +0900

    revert delete

---
+++ src/TableViewerBlazor/Internal/TeComponent/TeCheckBox.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace TableViewerBlazor.Internal.TeComponent;
+
+public partial class TeCheckBox : TeEditorBase
+{
+    [Parameter] public ITeCheckBoxOption CheckBoxOption { get; set; } = default!;
+    [Parameter] public object Parent { get; set; } = default!;
+
+    private bool isChecked = false;
+    private string CheckBoxLabel = string.Empty;
+
+    protected override void OnInitialized()
+    {
+        if (Data is bool boolValue)
+        {
+            isChecked = boolValue;
+            CheckBoxLabel = GetLabel(isChecked);
+        }
+    }
+
+    public async Task OnValueChanged(bool value)
+    {
+        isChecked = value;
+        CheckBoxLabel = GetLabel(isChecked);
+        await DataChanged.InvokeAsync(value);
+    }
+
+    private string GetLabel(bool isChecked)
+    {
+        var labelOption = CheckBoxOption.Property?.LabelOptions;
+        if (labelOption?.Condition(Parent) ?? false)
+        {
+            if (isChecked)
+            {
+                return labelOption.SelectedLabel(isChecked, Parent);
+            }
+            else
+            {
+                return labelOption.NotSelectedLabel(isChecked, Parent);
+            }
+        }
+        return string.Empty;
+    }
+}
---
+++ src/TableViewerBlazor/Options/TeCheckBoxOption.cs
@@ -0,0 +1,70 @@
+﻿using TableViewerBlazor.Internal.TeComponent;
+using TableViewerBlazor.Options.Property;
+
+namespace TableViewerBlazor.Options;
+
+public static class TeCheckBoxOptionExtensions
+{
+    public static bool TryGetCheckBoxOption(
+        this TeOptions options,
+        MemberInfo? memberInfo,
+        TeEditorBase teBase,
+        out ITeCheckBoxOption? checkBoxOption
+        )
+    {
+        var selectedAttribute = memberInfo?.GetCustomAttribute<TeCheckBoxAttribute>();
+        if (selectedAttribute != null)
+        {
+            checkBoxOption = options.CheckBoxOptions?
+                .FirstOrDefault(o => o.Id == selectedAttribute.Id) ?? default;
+            if (checkBoxOption != null)
+            {
+                return true;
+            }
+        }
+        checkBoxOption = options.CheckBoxOptions?
+            .Where(option => string.IsNullOrEmpty(option.Id))
+            .Where(option => option.Condition?.Invoke(teBase.Data, teBase.Depth, teBase.Path) ?? true)
+            .FirstOrDefault() ?? default!;
+
+        if (checkBoxOption != null)
+        {
+            return true;
+        }
+
+        checkBoxOption = teBase.Data switch
+        {
+            bool _ => new TeCheckBoxOption<object>(),
+            _ => null
+        };
+        return checkBoxOption != null;
+    }
+}
+
+public class TeCheckBoxAttribute : Attribute
+{
+    public string Id { get; init; }
+    public TeCheckBoxAttribute(string id)
+    {
+        Id = id;
+    }
+}
+
+public interface ITeCheckBoxOption : ITeFieldOption
+{
+    public ITeCheckBoxProperty Property { get; set; } 
+}
+
+public class TeCheckBoxOption<T> : ITeFieldOption<T>, ITeCheckBoxOption
+{
+    public string? Id { get; set; }
+    public Func<T?, int, string, bool>? Condition { get; set; }
+    public ITeCheckBoxProperty Property { get; set; } = new TeCheckBoxProperty();
+}
+
+public class TeCheckBoxOption : ITeCheckBoxOption
+{
+    public string? Id { get; set; }
+    public Func<object?, int, string, bool>? Condition { get; set; }
+    public ITeCheckBoxProperty Property { get; set; } = new TeCheckBoxProperty();
+}
