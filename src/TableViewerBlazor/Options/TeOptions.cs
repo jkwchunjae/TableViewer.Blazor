5caa06f136a519c207740a214e3f895c8008855a
Kyungwon Jeon <jkwchunjae@daum.net>
2024-08-03 17:00:38 +0900

    임시 삭제

--- src/TableViewerBlazor/Options/TeOptions.cs
+++ src/TableViewerBlazor/Options/TeOptions.cs
@@ -7,14 +7,9 @@ public class TeOptions
     public bool ReadField { get; set; } = true;
     public TeStyleOption Style { get; set; } = new();
     public List<ITeTextFieldOption> TextFieldOptions { get; set; } = [];
-    public List<ITeNumericFieldOption> NumericFieldOptions { get; set; } = [];
-    public List<ITeSelectBoxOption> SelectBoxOptions { get; set; } = [];
-    public List<ITeRadioOption> RadioOptions { get; set; } = [];
-    public List<ITeCheckBoxOption> CheckBoxOptions { get; set; } = [];
     public List<TeIgnoreOption> IgnoreOptions { get; set; } = [];
     public TeCustomEditorOptionGroup CustomEditorOptions { get; set; } = new();
     public List<TeObjectArrayEditorOption> ObjectArrayEditorOptions { get; set; } = [new()];
-    public List<ITeListEditorOption> ListEditorOptions { get; set; } = []; 
     public List<ITvButton> ToolbarButtons { get; set; } = [];
 }
 
