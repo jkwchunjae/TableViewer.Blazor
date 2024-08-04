daf4a51749bff637eaa986970caacbd0225ebc55
Kyungwon Jeon <jkwchunjae@daum.net>
2024-08-04 15:09:13 +0900

    test page

--- test/TableViewerTest/Components/Pages/EditorConverter.razor.cs
+++ test/TableViewerTest/Components/Pages/EditorConverter.razor.cs
@@ -32,6 +32,12 @@ public partial class EditorConverter : ComponentBase
             new TeSelectBoxOption<string>
             {
                 Id = nameof(StringTestData.String),
+                Items = new List<TeSelectItem<string>>
+                {
+                    new TeSelectItem<string> { Value = "A", Text = "A" },
+                    new TeSelectItem<string> { Value = "B", Text = "B" },
+                    new TeSelectItem<string> { Value = "C", Text = "C" },
+                },
                 // Converter = new TeCheckBoxConverter<string>
                 // {
                 //     FromBoolean = (b) => b.ToString(),
@@ -41,6 +47,12 @@ public partial class EditorConverter : ComponentBase
             new TeSelectBoxOption<int>
             {
                 Id = nameof(StringTestData.Number),
+                Items = new List<TeSelectItem<int>>
+                {
+                    new TeSelectItem<int> { Value = 1, Text = "1" },
+                    new TeSelectItem<int> { Value = 2, Text = "2" },
+                    new TeSelectItem<int> { Value = 3, Text = "3" },
+                },
                 // Converter = new TeCheckBoxConverter<int>
                 // {
                 //     FromBoolean = b => b ? 1 : 0,
@@ -50,6 +62,12 @@ public partial class EditorConverter : ComponentBase
             new TeSelectBoxOption<DateTime>
             {
                 Id = nameof(StringTestData.DateTime),
+                Items = new List<TeSelectItem<DateTime>>
+                {
+                    new TeSelectItem<DateTime> { Value = DateTime.MinValue, Text = "Min" },
+                    new TeSelectItem<DateTime> { Value = DateTime.MaxValue, Text = "Max" },
+                    new TeSelectItem<DateTime> { Value = DateTime.Now, Text = "Now" },
+                },
                 // Converter = new TeCheckBoxConverter<DateTime>
                 // {
                 //     FromBoolean = b => b ? DateTime.MaxValue : DateTime.MinValue,
@@ -59,10 +77,21 @@ public partial class EditorConverter : ComponentBase
             new TeSelectBoxOption<bool>
             {
                 Id = nameof(StringTestData.Bool),
+                Items = new List<TeSelectItem<bool>>
+                {
+                    new TeSelectItem<bool> { Value = true, Text = "True" },
+                    new TeSelectItem<bool> { Value = false, Text = "False" },
+                },
             },
             new TeSelectBoxOption<TestEnum>
             {
                 Id = nameof(StringTestData.Enum),
+                Items = new List<TeSelectItem<TestEnum>>
+                {
+                    new TeSelectItem<TestEnum> { Value = TestEnum.A, Text = "A" },
+                    new TeSelectItem<TestEnum> { Value = TestEnum.B, Text = "B" },
+                    new TeSelectItem<TestEnum> { Value = TestEnum.C, Text = "C" },
+                },
                 // Converter = new TeCheckBoxConverter<TestEnum>
                 // {
                 //     FromBoolean = (b) => b ? TestEnum.B : TestEnum.C,
@@ -72,6 +101,11 @@ public partial class EditorConverter : ComponentBase
             new TeSelectBoxOption<List<int>>
             {
                 Id = nameof(StringTestData.IntList),
+                Items = new List<TeSelectItem<List<int>>>
+                {
+                    new TeSelectItem<List<int>> { Value = new List<int> { 1, 2, 3, 4, 5 }, Text = "1, 2, 3, 4, 5" },
+                    new TeSelectItem<List<int>> { Value = new List<int> { 6, 7, 8, 9, 10 }, Text = "6, 7, 8, 9, 10" },
+                },
                 // Converter = new TeCheckBoxConverter<List<int>>
                 // {
                 //     FromBoolean = b => b ? new List<int> { 1, 2, 3, 4, 5 } : new List<int>(),
