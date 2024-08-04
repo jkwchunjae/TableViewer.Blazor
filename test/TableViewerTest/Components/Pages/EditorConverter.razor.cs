23530580d72e37ce5ba365e38718f6cc047c3577
Kyungwon Jeon <jkwchunjae@daum.net>
2024-08-04 23:51:34 +0900

    test page

--- test/TableViewerTest/Components/Pages/EditorConverter.razor.cs
+++ test/TableViewerTest/Components/Pages/EditorConverter.razor.cs
@@ -6,17 +6,17 @@ namespace TableViewerTest.Components.Pages;
 
 public class StringTestData
 {
-    [TeNumericField(nameof(String))]
+    [TeRadio(nameof(String))]
     public string String { get; set; } = string.Empty;
-    [TeNumericField(nameof(Number))]
+    [TeRadio(nameof(Number))]
     public int Number { get; set; }
-    [TeNumericField(nameof(DateTime))]
+    [TeRadio(nameof(DateTime))]
     public DateTime DateTime { get; set; }
-    [TeNumericField(nameof(Bool))]
+    [TeRadio(nameof(Bool))]
     public bool Bool { get; set; } = true;
-    [TeNumericField(nameof(Enum))]
+    [TeRadio(nameof(Enum))]
     public TestEnum Enum { get; set; }
-    [TeNumericField(nameof(IntList))]
+    [TeRadio(nameof(IntList))]
     public List<int> IntList { get; set; } = new List<int> { 1, 2, 3, 4, 5 };
 }
 
@@ -27,95 +27,95 @@ public partial class EditorConverter : ComponentBase
     StringTestData data = new StringTestData();
     TeOptions options = new TeOptions
     {
-        NumericFieldOptions =
+        RadioOptions =
         {
-            new TeNumericFieldOption<string, int>
+            new TeRadioOption<string>
             {
                 Id = nameof(StringTestData.String),
-                // Items = new List<TeSelectItem<string>>
-                // {
-                //     new TeSelectItem<string> { Value = "A", Text = "A" },
-                //     new TeSelectItem<string> { Value = "B", Text = "B" },
-                //     new TeSelectItem<string> { Value = "C", Text = "C" },
-                // },
-                Converter = new TeNumericFieldConverter<string, int>
+                Items = new List<TeRadioItem<string>>
                 {
-                    FromNumber = (b) => b.ToString(),
-                    ToNumber = (s) => int.TryParse(s, out var number) ? number : default,
+                    new TeRadioItem<string> { Value = "A", Text = "A" },
+                    new TeRadioItem<string> { Value = "B", Text = "B" },
+                    new TeRadioItem<string> { Value = "C", Text = "C" },
                 },
+                // Converter = new TeNumericFieldConverter<string, int>
+                // {
+                //     FromNumber = (b) => b.ToString(),
+                //     ToNumber = (s) => int.TryParse(s, out var number) ? number : default,
+                // },
             },
-            new TeNumericFieldOption<int>
+            new TeRadioOption<int>
             {
                 Id = nameof(StringTestData.Number),
-                // Items = new List<TeSelectItem<int>>
-                // {
-                //     new TeSelectItem<int> { Value = 1, Text = "1" },
-                //     new TeSelectItem<int> { Value = 2, Text = "2" },
-                //     new TeSelectItem<int> { Value = 3, Text = "3" },
-                // },
+                Items = new List<TeRadioItem<int>>
+                {
+                    new TeRadioItem<int> { Value = 1, Text = "1" },
+                    new TeRadioItem<int> { Value = 2, Text = "2" },
+                    new TeRadioItem<int> { Value = 3, Text = "3" },
+                },
                 // Converter = new TeNumericFieldConverter<int>
                 // {
                 //     FromNumber = b => b ? 1 : 0,
                 //     ToNumber = (i) => i == 0 ? false : true,
                 // },
             },
-            new TeNumericFieldOption<DateTime, double>
+            new TeRadioOption<DateTime>
             {
                 Id = nameof(StringTestData.DateTime),
-                // Items = new List<TeSelectItem<DateTime>>
-                // {
-                //     new TeSelectItem<DateTime> { Value = DateTime.MinValue, Text = "Min" },
-                //     new TeSelectItem<DateTime> { Value = DateTime.MaxValue, Text = "Max" },
-                //     new TeSelectItem<DateTime> { Value = DateTime.Now, Text = "Now" },
-                // },
-                Converter = new TeNumericFieldConverter<DateTime, double>
+                Items = new List<TeRadioItem<DateTime>>
                 {
-                    FromNumber = b => DateTime.FromOADate(b),
-                    ToNumber = (d) => d.ToOADate(),
+                    new TeRadioItem<DateTime> { Value = DateTime.MinValue, Text = "Min" },
+                    new TeRadioItem<DateTime> { Value = DateTime.MaxValue, Text = "Max" },
+                    new TeRadioItem<DateTime> { Value = DateTime.Now, Text = "Now" },
                 },
+                // Converter = new TeNumericFieldConverter<DateTime, double>
+                // {
+                //     FromNumber = b => DateTime.FromOADate(b),
+                //     ToNumber = (d) => d.ToOADate(),
+                // },
             },
-            new TeNumericFieldOption<bool, long>
+            new TeRadioOption<bool>
             {
                 Id = nameof(StringTestData.Bool),
-                // Items = new List<TeSelectItem<bool>>
-                // {
-                //     new TeSelectItem<bool> { Value = true, Text = "True" },
-                //     new TeSelectItem<bool> { Value = false, Text = "False" },
-                // },
-                Converter = new TeNumericFieldConverter<bool, long>
+                Items = new List<TeRadioItem<bool>>
                 {
-                    FromNumber = (l) => l == 0 ? false : true,
-                    ToNumber = b => b ? 1 : 0,
+                    new TeRadioItem<bool> { Value = true, Text = "True" },
+                    new TeRadioItem<bool> { Value = false, Text = "False" },
                 },
+                // Converter = new TeNumericFieldConverter<bool, long>
+                // {
+                //     FromNumber = (l) => l == 0 ? false : true,
+                //     ToNumber = b => b ? 1 : 0,
+                // },
             },
-            new TeNumericFieldOption<TestEnum, byte>
+            new TeRadioOption<TestEnum>
             {
                 Id = nameof(StringTestData.Enum),
-                // Items = new List<TeSelectItem<TestEnum>>
-                // {
-                //     new TeSelectItem<TestEnum> { Value = TestEnum.A, Text = "A" },
-                //     new TeSelectItem<TestEnum> { Value = TestEnum.B, Text = "B" },
-                //     new TeSelectItem<TestEnum> { Value = TestEnum.C, Text = "C" },
-                // },
-                Converter = new TeNumericFieldConverter<TestEnum, byte>
+                Items = new List<TeRadioItem<TestEnum>>
                 {
-                    FromNumber = (b) => (TestEnum)b,
-                    ToNumber = (e) => (byte)e,
+                    new TeRadioItem<TestEnum> { Value = TestEnum.A, Text = "A" },
+                    new TeRadioItem<TestEnum> { Value = TestEnum.B, Text = "B" },
+                    new TeRadioItem<TestEnum> { Value = TestEnum.C, Text = "C" },
                 },
+                // Converter = new TeNumericFieldConverter<TestEnum, byte>
+                // {
+                //     FromNumber = (b) => (TestEnum)b,
+                //     ToNumber = (e) => (byte)e,
+                // },
             },
-            new TeNumericFieldOption<List<int>, ulong>
+            new TeRadioOption<List<int>>
             {
                 Id = nameof(StringTestData.IntList),
-                // Items = new List<TeSelectItem<List<int>>>
-                // {
-                //     new TeSelectItem<List<int>> { Value = new List<int> { 1, 2, 3, 4, 5 }, Text = "1, 2, 3, 4, 5" },
-                //     new TeSelectItem<List<int>> { Value = new List<int> { 6, 7, 8, 9, 10 }, Text = "6, 7, 8, 9, 10" },
-                // },
-                Converter = new TeNumericFieldConverter<List<int>, ulong>
+                Items = new List<TeRadioItem<List<int>>>
                 {
-                    FromNumber = number => number.ToString().Select(x => int.Parse($"{x}")).ToList(),
-                    ToNumber = list => ulong.Parse(string.Join("", list)),
+                    new TeRadioItem<List<int>> { Value = new List<int> { 1, 2, 3, 4, 5 }, Text = "1, 2, 3, 4, 5" },
+                    new TeRadioItem<List<int>> { Value = new List<int> { 6, 7, 8, 9, 10 }, Text = "6, 7, 8, 9, 10" },
                 },
+                // Converter = new TeNumericFieldConverter<List<int>, ulong>
+                // {
+                //     FromNumber = number => number.ToString().Select(x => int.Parse($"{x}")).ToList(),
+                //     ToNumber = list => ulong.Parse(string.Join("", list)),
+                // },
             },
         },
     };
