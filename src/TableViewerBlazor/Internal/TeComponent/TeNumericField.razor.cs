c17145bd3cdc7b0fdca84df0bd22d1180d2659e0
Kyungwon Jeon <jkwchunjae@daum.net>
2024-08-04 19:52:33 +0900

    TeNumericField

--- src/TableViewerBlazor/Internal/TeComponent/TeNumericField.razor.cs
+++ src/TableViewerBlazor/Internal/TeComponent/TeNumericField.razor.cs
@@ -1,30 +1,45 @@
 ﻿using System.Numerics;
-using TableViewerBlazor.Options;
 using TableViewerBlazor.Options.Property;
 
 namespace TableViewerBlazor.Internal.TeComponent;
 
-public partial class TeNumericField : TeEditorBase
+public partial class TeNumericField<TNumber> : TeEditorBase
+    where TNumber : INumber<TNumber>, IMinMaxValue<TNumber>
 {
     [Parameter] public ITeNumericFieldOption NumericFieldOption { get; set; } = default!;
+    
+    TeNumericFieldProperty<TNumber> property = default!;
 
-    private async Task OnValueChanged(object? value)
+    TNumber numberValue = TNumber.Zero;
+    protected override void OnInitialized()
     {
-        Data = value;
+        var converted = NumericFieldOption.Converter.ToField(Data!);
+        if (converted is TNumber numberValue)
+        {
+            this.numberValue = numberValue;
+        }
+
+        property = GetNumberFieldProperty();
+    }
+
+    private async Task OnValueChanged(TNumber value)
+    {
+        Data = NumericFieldOption.Converter.FromField(value);
         await DataChanged.InvokeAsync(value);
     }
 
-    private async Task<IEnumerable<string>> NumericFieldValidation<T>(T value)
+    private async Task<IEnumerable<string>> NumericFieldValidation(TNumber value)
     {
+        var data = NumericFieldOption.Converter.FromField(value);
         var errors = new List<string>();
-        if (NumericFieldOption?.Validations != null)
+        if (NumericFieldOption?.Validations != null && data != null)
         {
             // Do not use Task.WhenAll
             foreach (var validation in NumericFieldOption.Validations)
             {
                 try
                 {
-                    if (!await validation.Func(value!))
+                    if (!await validation.Func(data))
                     {
                         errors.Add(validation.Message);
                     }
@@ -38,16 +53,15 @@ public partial class TeNumericField : TeEditorBase
         return errors;
     }
 
-    private TeNumericFieldProperty<T> GetNumberFieldProperty<T>()
-        where T : INumber<T>, IMinMaxValue<T>
+    private TeNumericFieldProperty<TNumber> GetNumberFieldProperty()
     {
-        if (NumericFieldOption.Property is TeNumericFieldProperty<T> property)
+        if (NumericFieldOption.Property is TeNumericFieldProperty<TNumber> property)
         {
             return property;
         }
         else
         {
-            return new TeNumericFieldProperty<T>();
+            return new TeNumericFieldProperty<TNumber>();
         }
     }
 }
