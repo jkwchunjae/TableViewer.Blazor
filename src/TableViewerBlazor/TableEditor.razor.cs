352d3527bf9c0da46d7ff8aec40a849a3f64fd1a
Kangho Dean Kim <88331311+ariaskkh@users.noreply.github.com>
2024-09-09 14:36:37 +0900

    TableEditor에서 CustomTableEditorBase 상속 받도록 수정

--- src/TableViewerBlazor/TableEditor.razor.cs
+++ src/TableViewerBlazor/TableEditor.razor.cs
@@ -1,42 +1,8 @@
-﻿namespace TableViewerBlazor;
+﻿using TableViewerBlazor.Internal.TeComponent.CustomTableEditor;
 
-public partial class TableEditor<T> : ComponentBase
+namespace TableViewerBlazor;
+
+public partial class TableEditor<T> : CustomTableEditorBase<T>
     where T : class
 {
-    [Parameter] public T Data { get; set; } = default!;
-    [Parameter] public EventCallback<T> DataChanged { get; set; }
-    [Parameter] public TeOptions Options { get; set; } = new TeOptions();
-    [Parameter] public EventCallback<bool> IsValidChanged { get; set; }
-    [Parameter] public EventCallback<string[]> ErrorsChanged { get; set; }
-
-    MudForm form = default!;
-    bool success;
-    string[] errors = { };
-
-    private Task OnValidChanged(bool success)
-    {
-        this.success = success;
-        return IsValidChanged.InvokeAsync(success);
-    }
-
-    private Task OnDataChanged(object? data)
-    {
-        if (data is T newData)
-        {
-            Data = newData;
-            return DataChanged.InvokeAsync(newData);
-        }
-        return Task.CompletedTask;
-    }
-
-    private async Task OnErrorsChanged(string[] errors)
-    {
-        this.errors = errors;
-        await ErrorsChanged.InvokeAsync(errors);
-    }
-
-    public Task Validate()
-    {
-        return form.Validate();
-    }
 }
