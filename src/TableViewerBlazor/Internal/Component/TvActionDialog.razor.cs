d03b87bb90d2af2bd05e34715f2770bcfb288d5d
Kangho Dean Kim <88331311+ariaskkh@users.noreply.github.com>
2024-09-11 12:55:10 +0900

    TvActionButton 생성

---
+++ src/TableViewerBlazor/Internal/Component/TvActionDialog.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace TableViewerBlazor.Internal.Component;
+
+// TODO: generic으로 받아야하는지 고민
+public partial class TvActionDialog : ComponentBase
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; } = null!;
+
+    [Parameter] public object Item { get; set; } = null!;
+    [Parameter] public ITvPopupAction PopupAction { get; set; } = null!;
+
+    private void OnClose() => MudDialog.Close();
+
+    private void OnClickButton()
+    {
+        PopupAction.Action(Item);
+    }
+}
---
+++ src/TableViewerBlazor/Options/TvPopupAction.cs
@@ -0,0 +1,62 @@
+﻿namespace TableViewerBlazor.Options;
+
+public interface ITvPopupAction : ITvAction
+{
+    Func<object, string> PopupTitle { get; }
+    Func<object, string> PopupContent { get; }
+    DialogOptions PopupStyle { get; }
+    PopupInnerButtonOptions<object> PopupInnerButtonOptions { get; }
+}
+
+public class TvPopupAction<T> : TvAction<T>, ITvPopupAction
+{
+    public Func<T, string> PopupTitle { get; set; } = _ => string.Empty;
+    public Func<T, string> PopupContent { get; set; } = _ => string.Empty;
+    public DialogOptions PopupStyle { get; set; } = new()
+    { 
+        FullWidth = true,
+        CloseOnEscapeKey = true,
+        NoHeader = false,
+    };
+    public PopupInnerButtonOptions<T> PopupInnerButtonOptions { get; set; } = new();
+
+    Func<object, string> ITvPopupAction.PopupTitle => o => o is T t ? PopupTitle(t) : string.Empty;
+
+    Func<object, string> ITvPopupAction.PopupContent => o => o is T t ? PopupContent(t) : string.Empty;
+
+    PopupInnerButtonOptions<object> ITvPopupAction.PopupInnerButtonOptions => new PopupInnerButtonOptions<object>
+    {
+        CloseButton = new TvAction<object>
+        {
+            Action = o => o is T t ? PopupInnerButtonOptions.CloseButton.Action(t) : Task.CompletedTask,
+            Label = PopupInnerButtonOptions.CloseButton.Label,
+            Condition = (o, i) => o is T t? PopupInnerButtonOptions.CloseButton.Condition(t, i) : false,
+            Style = PopupInnerButtonOptions.CloseButton.Style,
+        },
+        Buttons = PopupInnerButtonOptions.Buttons
+            .Select(button => new TvAction<object>
+            {
+                Action = o => o is T t ? button.Action(t) : Task.CompletedTask,
+                Label = button.Label,
+                Condition = (o, i) => o is T t ? button.Condition(t, i) : false,
+                Style = button.Style,
+                LabelAfterClick = button.LabelAfterClick,
+            }).ToList()
+    };
+}
+
+public class PopupInnerButtonOptions<T>
+{
+    public TvAction<T> CloseButton { get; set; } = new TvAction<T>
+    {
+        Action = _ => Task.CompletedTask,
+        Label = "Close",
+        Condition = (_, _) => true,
+        Style =
+        {
+            Color = Color.Default,
+            Variant = Variant.Outlined,
+        }
+    };
+    public List<TvAction<T>> Buttons { get; set; } = [];
+}
